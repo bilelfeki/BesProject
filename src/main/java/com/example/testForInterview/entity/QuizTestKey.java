e1f8e23a30e50688ed558f7512f3af933d282d4c
bile feki <bilel.feki.92@gmail.com>
2022-11-26 20:27:22 +0000

    create composite key

---
+++ src/main/java/com/example/testForInterview/entity/QuizTestKey.java
@@ -0,0 +1,36 @@
+package com.example.testForInterview.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Embeddable
+public class QuizTestKey implements Serializable {
+    @Column(name="quiz_id")
+    Long QuizId;
+    @Column(name="candidate_id")
+    Long CandidateId ;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof QuizTestKey)) return false;
+        QuizTestKey that = (QuizTestKey) o;
+        return Objects.equals(QuizId, that.QuizId) && Objects.equals(CandidateId, that.CandidateId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(QuizId, CandidateId);
+    }
+}
