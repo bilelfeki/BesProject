5723da1283621c08d817478a798c3a430a8b99d3
bile feki <bilel.feki.92@gmail.com>
2022-11-26 20:29:36 +0000

    initial commit

---
+++ src/main/java/com/example/testForInterview/TestForInterviewApplication.java
@@ -0,0 +1,12 @@
+package com.example.testForInterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestForInterviewApplication.class, args);
+	}
+}
